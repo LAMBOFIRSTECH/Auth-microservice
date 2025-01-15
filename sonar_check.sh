ca6bff46e4e50769354f3ea7411d414f96772ffd
snow <lamboartur94@gmail.com>
2025-01-15 11:11:52 +0100

    updates files

--- sonar_check.sh
+++ sonar_check.sh
@@ -97,9 +97,13 @@ dotnet sonarscanner begin \
 # --------------------
 # 5. Restauration du projet
 # ------------------------
-dotnet restore "$SOLUTION_FILE"
+#dotnet restore "$SOLUTION_FILE"
 colors "YELLOW" "Restauration du projet terminée"
+# Exécution des tests avec couverture
+dotnet test "$SOLUTION_FILE" --configuration Release --no-restore
 
+# Compilation du projet
+dotnet build "$SOLUTION_FILE" --configuration Release --no-restore
 # --------------------
 # 5. Compilation du Projet
 # --------------------
