e584c68fb62967e299a4fd6c3a30b3d4d5e02f04
snow <lamboartur94@gmail.com>
2025-01-20 04:27:03 +0100

    Get artéfacts

--- Authentifications/Interfaces/IRedisCacheTokenService.cs
+++ Authentifications/Interfaces/IRedisCacheTokenService.cs
@@ -1,9 +1,6 @@
-using Authentifications.Models;
-
 namespace Authentifications.Interfaces;
 public interface IRedisCacheTokenService
 {
-    byte[] ComputeHashUsingByte(string email, string password);
     void StoreRefreshTokenSessionInRedis(string email, string token, string password);
     Task<string> RetrieveTokenBasingOnRedisUserSessionAsync(string email, string password);
 }
