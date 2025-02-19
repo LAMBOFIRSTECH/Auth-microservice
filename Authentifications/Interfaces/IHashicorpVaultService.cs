45b565f534a97cfba681343fccf9bc37b8537779
Artur-Lambo <85019387+Artur-Lambo@users.noreply.github.com>
2025-02-20 00:48:07 +0100

    Update IHashicorpVaultService.cs

--- Authentifications/Interfaces/IHashicorpVaultService.cs
+++ Authentifications/Interfaces/IHashicorpVaultService.cs
@@ -1,6 +1,7 @@
+using Authentifications.Models;
 namespace Authentifications.Interfaces;
 public interface IHashicorpVaultService
 {
     Task<string> GetRabbitConnectionStringFromVault();
-    void StoreJwtPublicKeyInVault(string publicKeyPem);
+    Task<Message> StoreJwtPublicKeyInVault(string publicKeyPem);
 }
