6ea6faff5485183909fe2819c5b11960ff0950e5
Artur-Lambo <85019387+Artur-Lambo@users.noreply.github.com>
2025-02-20 00:49:14 +0100

    Update IRedisCacheService.cs

--- Authentifications/Interfaces/IRedisCacheService.cs
+++ Authentifications/Interfaces/IRedisCacheService.cs
@@ -2,7 +2,8 @@ using Authentifications.Models;
 namespace Authentifications.Interfaces;
 public interface IRedisCacheService
 {
-	Task<ICollection<UtilisateurDto>> RetrieveDataOnRedisUsingKeyAsync();
-	Task<ICollection<UtilisateurDto>> ValidateAndSyncDataAsync(string cachedData);
+	Task<ICollection<UtilisateurDto>> HangFireRetrieveDataOnRedisUsingKeyAsync();
+	Task<ICollection<UtilisateurDto>> ValidateAndSyncRedisDataAsync(string cachedData);
 	Task<(bool, UtilisateurDto)> GetBooleanAndUserDataFromRedisUsingParamsAsync(bool condition, string email, string password);
+	Task<ICollection<UtilisateurDto>> RetrieveDataFromExternalApiAsync();
 }
