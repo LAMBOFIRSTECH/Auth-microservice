d0b45b48224be94c6d6f45f0c7aaa61127a80a0b
Artur-Lambo <85019387+Artur-Lambo@users.noreply.github.com>
2025-02-20 00:53:33 +0100

    Create Message.cs

---
+++ Authentifications/Models/Message.cs
@@ -0,0 +1,8 @@
+namespace Authentifications.Models;
+public class Message
+{
+    public string? Type { get; set; }
+    public string? Title { get; set; }
+    public string? Detail { get; set; }
+    public int Status { get; set; }
+}
